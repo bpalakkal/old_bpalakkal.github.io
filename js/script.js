80d22120ca5b1142264b011cbb792790b0461c54
bpalakkal <47097307+bpalakkal@users.noreply.github.com>
2019-07-28 07:51:00 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -117,7 +117,7 @@ var tabulate = function (data,columns) {
 }
 
 d3.csv('data.csv',function (data) {
-	var columns = ['Name','Team','Position','STL']
+	var columns = ['Name','Team','Position','SPG']
   tabulate(data,columns)
 })
 
@@ -158,7 +158,7 @@ var tabulate = function (data,columns) {
 }
 
 d3.csv('data.csv',function (data) {
-	var columns = ['Name','Team','Position','AST']
+	var columns = ['Name','Team','Position','APG']
   tabulate(data,columns)
 })
 
@@ -199,6 +199,47 @@ var tabulate = function (data,columns) {
 }
 
 d3.csv('data.csv',function (data) {
-	var columns = ['Name','Team','Position','BLK']
+	var columns = ['Name','Team','Position','BPG']
+  tabulate(data,columns)
+})
+
+var tabulate = function (data,columns) {
+    var svg = d3.select("body").append("svg")
+      .attr("width", 500)
+      .attr("height", 300);
+    var forobj = svg.append("foreignObject")
+      .attr("width", 400)
+      .attr("height", 200)
+      .append("xhtml:body")
+    var table = forobj.append("table")    
+    var thead = table.append('thead')
+    var tbody = table.append('tbody')
+	thead.append('tr')
+	  .selectAll('th')
+	    .data(columns)
+	    .enter()
+	  .append('th')
+	    .text(function (d) { return d })
+
+	var rows = tbody.selectAll('tr')
+	    .data(data)
+	    .enter()
+	  .append('tr')
+
+	var cells = rows.selectAll('td')
+	    .data(function(row) {
+	    	return columns.map(function (column) {
+	    		return { column: column, value: row[column] }
+	      })
+      })
+      .enter()
+    .append('td')
+      .text(function (d) { return d.value })
+
+  return table;
+}
+
+d3.csv('data.csv',function (data) {
+	var columns = ['Name','Team','Position','FGPt']
   tabulate(data,columns)
 })
