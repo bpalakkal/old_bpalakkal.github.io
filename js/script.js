4a7b0a72643772fdd976519102378568718be3d3
bpalakkal <47097307+bpalakkal@users.noreply.github.com>
2019-07-28 07:39:22 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -38,3 +38,44 @@ d3.csv('data.csv',function (data) {
 	var columns = ['Name','Team','Position','PPG']
   tabulate(data,columns)
 })
+
+var tabulate = function (data,columns) {
+    var svg = d3.select("body").append("svg")
+      .attr("width", 960)
+      .attr("height", 500);
+    var forobj = svg.append("foreignObject")
+      .attr("width", 480)
+      .attr("height", 500)
+      .append("xhtml:body")
+    var table = forobj.append("table")    
+    var thead = table.append('thead')
+    var tbody = table.append('tbody')
+	thead.append('tr')
+	  .selectAll('th')
+	    .data(columns)
+	    .enter()
+	  .append('th')
+	    .text(function (d) { return d })
+
+	var rows = tbody.selectAll('tr')
+	    .data(data)
+	    .enter()
+	  .append('tr')
+
+	var cells = rows.selectAll('td')
+	    .data(function(row) {
+	    	return columns.map(function (column) {
+	    		return { column: column, value: row[column] }
+	      })
+      })
+      .enter()
+    .append('td')
+      .text(function (d) { return d.value })
+
+  return table;
+}
+
+d3.csv('data.csv',function (data) {
+	var columns = ['Name','Team','Position','RPG']
+  tabulate(data,columns)
+})
