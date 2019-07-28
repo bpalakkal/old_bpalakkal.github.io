edefcbef2c7f983d195b7dbf399a455cc27c6c1e
bpalakkal <47097307+bpalakkal@users.noreply.github.com>
2019-07-28 12:35:13 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ var tabulate = function (data,columns) {
       .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
     var svgtext = svg.append("text")
         .attr("x",(width/2))
-    	.attr("y",0-(margin.top/2))
+    	.attr("y",0-(margin.top/4))
     	.style("text-anchor","middle")
     	.style("font-size","16px")
     	.style("text-decoration","underline")
@@ -16,7 +16,7 @@ var tabulate = function (data,columns) {
     	.text("Points per Game")    
     
     var forobj = svg.append("foreignObject")
-      .attr("width", 400)
+      .attr("width", 300)
       .attr("height", 200)
       .append("xhtml:body")
     var table = forobj.append("table")
@@ -51,3 +51,442 @@ d3.csv('data.csv',function (data) {
 	var columns = ['Name','Team','Position','PPG']
   tabulate(data,columns)
 })
+
+var tabulate = function (data,columns) {
+    var margin = {top: 20, right: 10, bottom: 20, left: 10}
+    var width = 500 - margin.left - margin.right, height = 300 - margin.top - margin.bottom	
+    var svg = d3.select("body").append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)    
+      .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+    var svgtext = svg.append("text")
+        .attr("x",(width/2))
+    	.attr("y",0-(margin.top/4))
+    	.style("text-anchor","middle")
+    	.style("font-size","16px")
+    	.style("text-decoration","underline")
+    	.style("font-weight","bold")
+    	.text("Rebounds per Game")    
+    
+    var forobj = svg.append("foreignObject")
+      .attr("width", 300)
+      .attr("height", 200)
+      .append("xhtml:body")
+    var table = forobj.append("table")
+    var thead = table.append('thead')
+    var tbody = table.append('tbody')
+	thead.append('tr')
+	  .selectAll('th')
+	    .data(columns)
+	    .enter()
+	  .append('th')
+	    .text(function (d) { return d })
+
+	var rows = tbody.selectAll('tr')
+	    .data(data)
+	    .enter()
+	  .append('tr')
+
+	var cells = rows.selectAll('td')
+	    .data(function(row) {
+	    	return columns.map(function (column) {
+	    		return { column: column, value: row[column] }
+	      })
+      })
+      .enter()
+    .append('td')
+      .text(function (d) { return d.value })
+
+  return table;
+}
+
+d3.csv('data.csv',function (data) {
+	var columns = ['Name','Team','Position','RPG']
+  tabulate(data,columns)
+})
+
+
+var tabulate = function (data,columns) {
+    var margin = {top: 20, right: 10, bottom: 20, left: 10}
+    var width = 500 - margin.left - margin.right, height = 300 - margin.top - margin.bottom	
+    var svg = d3.select("body").append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)    
+      .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+    var svgtext = svg.append("text")
+        .attr("x",(width/2))
+    	.attr("y",0-(margin.top/4))
+    	.style("text-anchor","middle")
+    	.style("font-size","16px")
+    	.style("text-decoration","underline")
+    	.style("font-weight","bold")
+    	.text("Steals per Game")    
+    
+    var forobj = svg.append("foreignObject")
+      .attr("width", 300)
+      .attr("height", 200)
+      .append("xhtml:body")
+    var table = forobj.append("table")
+    var thead = table.append('thead')
+    var tbody = table.append('tbody')
+	thead.append('tr')
+	  .selectAll('th')
+	    .data(columns)
+	    .enter()
+	  .append('th')
+	    .text(function (d) { return d })
+
+	var rows = tbody.selectAll('tr')
+	    .data(data)
+	    .enter()
+	  .append('tr')
+
+	var cells = rows.selectAll('td')
+	    .data(function(row) {
+	    	return columns.map(function (column) {
+	    		return { column: column, value: row[column] }
+	      })
+      })
+      .enter()
+    .append('td')
+      .text(function (d) { return d.value })
+
+  return table;
+}
+
+d3.csv('data.csv',function (data) {
+	var columns = ['Name','Team','Position','SPG']
+  tabulate(data,columns)
+})
+
+
+var tabulate = function (data,columns) {
+    var margin = {top: 20, right: 10, bottom: 20, left: 10}
+    var width = 500 - margin.left - margin.right, height = 300 - margin.top - margin.bottom	
+    var svg = d3.select("body").append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)    
+      .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+    var svgtext = svg.append("text")
+        .attr("x",(width/2))
+    	.attr("y",0-(margin.top/4))
+    	.style("text-anchor","middle")
+    	.style("font-size","16px")
+    	.style("text-decoration","underline")
+    	.style("font-weight","bold")
+    	.text("Assists per Game")    
+    
+    var forobj = svg.append("foreignObject")
+      .attr("width", 300)
+      .attr("height", 200)
+      .append("xhtml:body")
+    var table = forobj.append("table")
+    var thead = table.append('thead')
+    var tbody = table.append('tbody')
+	thead.append('tr')
+	  .selectAll('th')
+	    .data(columns)
+	    .enter()
+	  .append('th')
+	    .text(function (d) { return d })
+
+	var rows = tbody.selectAll('tr')
+	    .data(data)
+	    .enter()
+	  .append('tr')
+
+	var cells = rows.selectAll('td')
+	    .data(function(row) {
+	    	return columns.map(function (column) {
+	    		return { column: column, value: row[column] }
+	      })
+      })
+      .enter()
+    .append('td')
+      .text(function (d) { return d.value })
+
+  return table;
+}
+
+d3.csv('data.csv',function (data) {
+	var columns = ['Name','Team','Position','APG']
+  tabulate(data,columns)
+})
+
+
+var tabulate = function (data,columns) {
+    var margin = {top: 20, right: 10, bottom: 20, left: 10}
+    var width = 500 - margin.left - margin.right, height = 300 - margin.top - margin.bottom	
+    var svg = d3.select("body").append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)    
+      .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+    var svgtext = svg.append("text")
+        .attr("x",(width/2))
+    	.attr("y",0-(margin.top/4))
+    	.style("text-anchor","middle")
+    	.style("font-size","16px")
+    	.style("text-decoration","underline")
+    	.style("font-weight","bold")
+    	.text("Offensive Rating")    
+    
+    var forobj = svg.append("foreignObject")
+      .attr("width", 300)
+      .attr("height", 200)
+      .append("xhtml:body")
+    var table = forobj.append("table")
+    var thead = table.append('thead')
+    var tbody = table.append('tbody')
+	thead.append('tr')
+	  .selectAll('th')
+	    .data(columns)
+	    .enter()
+	  .append('th')
+	    .text(function (d) { return d })
+
+	var rows = tbody.selectAll('tr')
+	    .data(data)
+	    .enter()
+	  .append('tr')
+
+	var cells = rows.selectAll('td')
+	    .data(function(row) {
+	    	return columns.map(function (column) {
+	    		return { column: column, value: row[column] }
+	      })
+      })
+      .enter()
+    .append('td')
+      .text(function (d) { return d.value })
+
+  return table;
+}
+
+d3.csv('data.csv',function (data) {
+	var columns = ['Name','Team','Position','OffRating']
+  tabulate(data,columns)
+})
+
+
+var tabulate = function (data,columns) {
+    var margin = {top: 20, right: 10, bottom: 20, left: 10}
+    var width = 500 - margin.left - margin.right, height = 300 - margin.top - margin.bottom	
+    var svg = d3.select("body").append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)    
+      .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+    var svgtext = svg.append("text")
+        .attr("x",(width/2))
+    	.attr("y",0-(margin.top/4))
+    	.style("text-anchor","middle")
+    	.style("font-size","16px")
+    	.style("text-decoration","underline")
+    	.style("font-weight","bold")
+    	.text("Defensive Rating")    
+    
+    var forobj = svg.append("foreignObject")
+      .attr("width", 300)
+      .attr("height", 200)
+      .append("xhtml:body")
+    var table = forobj.append("table")
+    var thead = table.append('thead')
+    var tbody = table.append('tbody')
+	thead.append('tr')
+	  .selectAll('th')
+	    .data(columns)
+	    .enter()
+	  .append('th')
+	    .text(function (d) { return d })
+
+	var rows = tbody.selectAll('tr')
+	    .data(data)
+	    .enter()
+	  .append('tr')
+
+	var cells = rows.selectAll('td')
+	    .data(function(row) {
+	    	return columns.map(function (column) {
+	    		return { column: column, value: row[column] }
+	      })
+      })
+      .enter()
+    .append('td')
+      .text(function (d) { return d.value })
+
+  return table;
+}
+
+d3.csv('data.csv',function (data) {
+	var columns = ['Name','Team','Position','DefRating']
+  tabulate(data,columns)
+})
+
+var tabulate = function (data,columns) {
+    var margin = {top: 20, right: 10, bottom: 20, left: 10}
+    var width = 500 - margin.left - margin.right, height = 300 - margin.top - margin.bottom	
+    var svg = d3.select("body").append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)    
+      .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+    var svgtext = svg.append("text")
+        .attr("x",(width/2))
+    	.attr("y",0-(margin.top/4))
+    	.style("text-anchor","middle")
+    	.style("font-size","16px")
+    	.style("text-decoration","underline")
+    	.style("font-weight","bold")
+    	.text("Minutes per Game")    
+    
+    var forobj = svg.append("foreignObject")
+      .attr("width", 300)
+      .attr("height", 200)
+      .append("xhtml:body")
+    var table = forobj.append("table")
+    var thead = table.append('thead')
+    var tbody = table.append('tbody')
+	thead.append('tr')
+	  .selectAll('th')
+	    .data(columns)
+	    .enter()
+	  .append('th')
+	    .text(function (d) { return d })
+
+	var rows = tbody.selectAll('tr')
+	    .data(data)
+	    .enter()
+	  .append('tr')
+
+	var cells = rows.selectAll('td')
+	    .data(function(row) {
+	    	return columns.map(function (column) {
+	    		return { column: column, value: row[column] }
+	      })
+      })
+      .enter()
+    .append('td')
+      .text(function (d) { return d.value })
+
+  return table;
+}
+
+d3.csv('data.csv',function (data) {
+	var columns = ['Name','Team','Position','MPG']
+  tabulate(data,columns)
+})
+
+
+var tabulate = function (data,columns) {
+    var margin = {top: 20, right: 10, bottom: 20, left: 10}
+    var width = 500 - margin.left - margin.right, height = 300 - margin.top - margin.bottom	
+    var svg = d3.select("body").append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)    
+      .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+    var svgtext = svg.append("text")
+        .attr("x",(width/2))
+    	.attr("y",0-(margin.top/4))
+    	.style("text-anchor","middle")
+    	.style("font-size","16px")
+    	.style("text-decoration","underline")
+    	.style("font-weight","bold")
+    	.text("Free Throw %")    
+    
+    var forobj = svg.append("foreignObject")
+      .attr("width", 300)
+      .attr("height", 200)
+      .append("xhtml:body")
+    var table = forobj.append("table")
+    var thead = table.append('thead')
+    var tbody = table.append('tbody')
+	thead.append('tr')
+	  .selectAll('th')
+	    .data(columns)
+	    .enter()
+	  .append('th')
+	    .text(function (d) { return d })
+
+	var rows = tbody.selectAll('tr')
+	    .data(data)
+	    .enter()
+	  .append('tr')
+
+	var cells = rows.selectAll('td')
+	    .data(function(row) {
+	    	return columns.map(function (column) {
+	    		return { column: column, value: row[column] }
+	      })
+      })
+      .enter()
+    .append('td')
+      .text(function (d) { return d.value })
+
+  return table;
+}
+
+d3.csv('data.csv',function (data) {
+	var columns = ['Name','Team','Position','FTPt']
+  tabulate(data,columns)
+})
+
+
+var tabulate = function (data,columns) {
+    var margin = {top: 20, right: 10, bottom: 20, left: 10}
+    var width = 500 - margin.left - margin.right, height = 300 - margin.top - margin.bottom	
+    var svg = d3.select("body").append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)    
+      .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+    var svgtext = svg.append("text")
+        .attr("x",(width/2))
+    	.attr("y",0-(margin.top/4))
+    	.style("text-anchor","middle")
+    	.style("font-size","16px")
+    	.style("text-decoration","underline")
+    	.style("font-weight","bold")
+    	.text("Field Goal %")    
+    
+    var forobj = svg.append("foreignObject")
+      .attr("width", 300)
+      .attr("height", 200)
+      .append("xhtml:body")
+    var table = forobj.append("table")
+    var thead = table.append('thead')
+    var tbody = table.append('tbody')
+	thead.append('tr')
+	  .selectAll('th')
+	    .data(columns)
+	    .enter()
+	  .append('th')
+	    .text(function (d) { return d })
+
+	var rows = tbody.selectAll('tr')
+	    .data(data)
+	    .enter()
+	  .append('tr')
+
+	var cells = rows.selectAll('td')
+	    .data(function(row) {
+	    	return columns.map(function (column) {
+	    		return { column: column, value: row[column] }
+	      })
+      })
+      .enter()
+    .append('td')
+      .text(function (d) { return d.value })
+
+  return table;
+}
+
+d3.csv('data.csv',function (data) {
+	var columns = ['Name','Team','Position','FGPt']
+  tabulate(data,columns)
+})
+
