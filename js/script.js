41baadc1951fecabc595abf50e917f7a3c2c4cc4
bpalakkal <47097307+bpalakkal@users.noreply.github.com>
2019-07-28 07:48:20 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 var tabulate = function (data,columns) {
     var svg = d3.select("body").append("svg")
       .attr("width", 500)
-      .attr("height", 500);
+      .attr("height", 300);
     var forobj = svg.append("foreignObject")
       .attr("width", 400)
-      .attr("height", 500)
+      .attr("height", 200)
       .append("xhtml:body")
     var table = forobj.append("table")    
     var thead = table.append('thead')
@@ -42,10 +42,10 @@ d3.csv('data.csv',function (data) {
 var tabulate = function (data,columns) {
     var svg = d3.select("body").append("svg")
       .attr("width", 500)
-      .attr("height", 500);
+      .attr("height", 300);
     var forobj = svg.append("foreignObject")
       .attr("width", 400)
-      .attr("height", 500)
+      .attr("height", 200)
       .append("xhtml:body")
     var table = forobj.append("table")    
     var thead = table.append('thead')
@@ -77,5 +77,128 @@ var tabulate = function (data,columns) {
 
 d3.csv('data.csv',function (data) {
 	var columns = ['Name','Team','Position','RPG']
+  tabulate(data,columns)
+})
+
+var tabulate = function (data,columns) {
+    var svg = d3.select("body").append("svg")
+      .attr("width", 500)
+      .attr("height", 300);
+    var forobj = svg.append("foreignObject")
+      .attr("width", 400)
+      .attr("height", 200)
+      .append("xhtml:body")
+    var table = forobj.append("table")    
+    var thead = table.append('thead')
+    var tbody = table.append('tbody')
+	thead.append('tr')
+	  .selectAll('th')
+	    .data(columns)
+	    .enter()
+	  .append('th')
+	    .text(function (d) { return d })
+
+	var rows = tbody.selectAll('tr')
+	    .data(data)
+	    .enter()
+	  .append('tr')
+
+	var cells = rows.selectAll('td')
+	    .data(function(row) {
+	    	return columns.map(function (column) {
+	    		return { column: column, value: row[column] }
+	      })
+      })
+      .enter()
+    .append('td')
+      .text(function (d) { return d.value })
+
+  return table;
+}
+
+d3.csv('data.csv',function (data) {
+	var columns = ['Name','Team','Position','STL']
+  tabulate(data,columns)
+})
+
+var tabulate = function (data,columns) {
+    var svg = d3.select("body").append("svg")
+      .attr("width", 500)
+      .attr("height", 300);
+    var forobj = svg.append("foreignObject")
+      .attr("width", 400)
+      .attr("height", 200)
+      .append("xhtml:body")
+    var table = forobj.append("table")    
+    var thead = table.append('thead')
+    var tbody = table.append('tbody')
+	thead.append('tr')
+	  .selectAll('th')
+	    .data(columns)
+	    .enter()
+	  .append('th')
+	    .text(function (d) { return d })
+
+	var rows = tbody.selectAll('tr')
+	    .data(data)
+	    .enter()
+	  .append('tr')
+
+	var cells = rows.selectAll('td')
+	    .data(function(row) {
+	    	return columns.map(function (column) {
+	    		return { column: column, value: row[column] }
+	      })
+      })
+      .enter()
+    .append('td')
+      .text(function (d) { return d.value })
+
+  return table;
+}
+
+d3.csv('data.csv',function (data) {
+	var columns = ['Name','Team','Position','AST']
+  tabulate(data,columns)
+})
+
+var tabulate = function (data,columns) {
+    var svg = d3.select("body").append("svg")
+      .attr("width", 500)
+      .attr("height", 300);
+    var forobj = svg.append("foreignObject")
+      .attr("width", 400)
+      .attr("height", 200)
+      .append("xhtml:body")
+    var table = forobj.append("table")    
+    var thead = table.append('thead')
+    var tbody = table.append('tbody')
+	thead.append('tr')
+	  .selectAll('th')
+	    .data(columns)
+	    .enter()
+	  .append('th')
+	    .text(function (d) { return d })
+
+	var rows = tbody.selectAll('tr')
+	    .data(data)
+	    .enter()
+	  .append('tr')
+
+	var cells = rows.selectAll('td')
+	    .data(function(row) {
+	    	return columns.map(function (column) {
+	    		return { column: column, value: row[column] }
+	      })
+      })
+      .enter()
+    .append('td')
+      .text(function (d) { return d.value })
+
+  return table;
+}
+
+d3.csv('data.csv',function (data) {
+	var columns = ['Name','Team','Position','BLK']
   tabulate(data,columns)
 })
