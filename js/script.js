c8f95cf8683565f3e59b8ff29c15aff962227116
bpalakkal <47097307+bpalakkal@users.noreply.github.com>
2019-07-28 19:26:04 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -31,7 +31,9 @@ var tabulate = function (data,columns) {
       .append("g")
       .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
      
-    var svgtext = svg.append("text")
+ var svgtext = svg.append("a")    	
+    	.attr("xlink:href", "ppg.html")
+        .append("text")
     	.attr("x",(width/2))
     	.attr("y",0-(margin.top/4))
     	.style("text-anchor","middle")
@@ -40,6 +42,8 @@ var tabulate = function (data,columns) {
     	.style("text-decoration","underline")
     	.style("font-weight","bold") 
     	.text("Points Per Game")
+	.on("mouseover", function(){return tooltip.style("visibility", "visible").style("top", (event.pageY-10)+"px").style("left",(event.pageX+10)+"px")})
+	.on("mouseout", function(){return tooltip.style("visibility", "hidden");});
 
 	
     var forobj = svg.append("foreignObject")
