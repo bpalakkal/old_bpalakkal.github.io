a46a506531e6b4acdd062b7d629e01de5df5ab7e
bpalakkal <47097307+bpalakkal@users.noreply.github.com>
2019-07-28 23:49:29 -0400

    Update script_ppg.js

--- js/script_ppg.js
+++ js/script_ppg.js
@@ -75,7 +75,7 @@ d3.csv('data.csv',function (data) {
 var margin = {top: 20, right: 20, bottom: 30, left: 400},
     width = 960 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
-var svg = d3.select("body")
+var svg = d3.select("body").append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
   .append("g")
