80db9fe51a9584e1979a7d88cde2b0450660d94d
bpalakkal <47097307+bpalakkal@users.noreply.github.com>
2019-07-28 23:24:19 -0400

    Update script_ppg.js

--- js/script_ppg.js
+++ js/script_ppg.js
@@ -71,92 +71,117 @@ d3.csv('data.csv',function (data) {
 		return d3.descending(parseFloat(a.PPG), parseFloat(b.PPG))})
 })
 
+fdata = d3.csv('data.csv',function (data) {
+	var columns = ['Name','Team','MPG','PPG']
+  	ppg = tabulate(data,columns)
+	ppg.select("tbody").selectAll("tr")	
+	.filter(function(d){
+		return d["Team"]==="GOL})
+})
 
-    var margin = {top: 100, right: 20, bottom: 30, left: 100},
-        width = 880 - margin.left - margin.right,
-        height = 500 - margin.top - margin.bottom;
+// Calling the method below  
+ showScatterPlot(fdata);  
+   
+function showScatterPlot(data)   
+ {  
+     // Spacing Around   
+     var margins =  
+     {  
+         "left": 40,  
+         "right": 30,  
+         "top": 30,  
+         "bottom": 30  
+     };  
+   
+     var width = 500;  
+     var height = 500;  
+   
+     // This will be our colour scale. An Ordinal scale.  
+     var colors = d3.scale.category10();  
+   
+     // Adding the SVG component to the scatter-load div  
+     var svg = d3.select("#scatter-load").append("svg").attr("width", width).attr("height", height).append("g")  
+     .attr("transform", "translate(" + margins.left + "," + margins.top + ")");  
+   
+     // Setting the scale that we're using for the X axis.   
+     // Domain define the min and max variables to show. In this case, it's the min and max Votes of items.  
+     // this is made a compact piece of code due to d3.extent which gives back the max and min of the Vote variable within the dataset  
+     var x = d3.scale.linear()  
+     .domain(d3.extent(data, function (d)   
+     {  
+         return d.MPG;  
+     }))  
+   
+     // Range maps the domain to values from 0 to the width minus the left and right margins (used to space out the visualization)  
+     .range([0, width - margins.left - margins.right]);  
+   
+     // Scalling for the y axis but maps from the rating variable to the height to 0.   
+     var y = d3.scale.linear()  
+     .domain(d3.extent(data, function (d)  
+     {  
+         return d.PPG;  
+     }))  
+   
+     // Note that height goes first due to the weird SVG coordinate system  
+     .range([height - margins.top - margins.bottom, 0]);  
+   
+     // Adding the axes SVG component. At this point, this is just a placeholder. The actual axis will be added in a bit  
+     svg.append("g").attr("class", "x axis").attr("transform", "translate(0," + y.range()[0] + ")");  
+     svg.append("g").attr("class", "y axis");  
+   
+     // X axis label. Nothing too special to see here.  
+     svg.append("text")  
+     .attr("fill", "#414241")  
+     .attr("text-anchor", "end")  
+     .attr("x", width / 2)  
+     .attr("y", height - 35)  
+     .text("MPG");  
+   
+   
+     // Actual definition of our x and y axes. The orientation refers to where the labels appear - for the x axis, below or above the line, and for the y axis, left or right of the line. Tick padding refers to how much space between the tick and the label. There are other parameters too - see https://github.com/mbostock/d3/wiki/SVG-Axes for more information  
+ var xAxis = d3.svg.axis().scale(x).orient("bottom").tickPadding(2);  
+ var yAxis = d3.svg.axis().scale(y).orient("left").tickPadding(2);  
+   
+     // Selecting the axis we created a few lines earlier. See how we select the axis item. in our svg we appended a g element with a x/y and axis class. To pull that back up, we do this svg select, then 'call' the appropriate axis object for rendering.   
+     svg.selectAll("g.y.axis").call(yAxis);  
+     svg.selectAll("g.x.axis").call(xAxis);  
+   
+     // Now, we can get down to the data part, and drawing stuff. We are telling D3 that all nodes (g elements with class node) will have data attached to them. The 'key' we use (to let D3 know the uniqueness of items) will be the name. Not usually a great key, but fine for this example.  
+     var chocolate = svg.selectAll("g.node").data(data, function (d)   
+     {  
+         return d.name;  
+     });  
+   
+     // We 'enter' the data, making the SVG group (to contain a circle and text) with a class node. This corresponds with what we told the data it should be above.  
+   
+     var chocolateGroup = chocolate.enter().append("g").attr("class", "node")  
+   
+     // this is how we set the position of the items. Translate is an incredibly useful function for rotating and positioning items   
+     .attr('transform', function (d)   
+     {  
+         return "translate(" + x(d.MPG) + "," + y(d.PPG) + ")";  
+     });  
+   
+     // Adding our first graphics element! A circle!   
+     chocolateGroup.append("circle")  
+     .attr("r", 5)  
+     .attr("class", "dot")  
+     .style("fill", function (d)  
+     {  
+         // remember the ordinal scales?   
+         // We use the colors scale to get a colour for our votes. Now each node will be coloured  
+         // by votes to the languages.   
+         return colors(d.Team);  
+     });  
+   
+     // Adding some text, so we can see what each item is.  
+     chocolateGroup.append("text")  
+     .style("text-anchor", "middle")  
+     .attr("dy", -10)  
+     .text(function (d)  
+     {  
+         // this shouldn't be a surprising statement.  
+         return d.Name;  
+     });  
+ }  
 
-    var x = d3.scale.linear()
-        .range([0, width]);
-
-    var y = d3.scale.linear()
-        .range([height, 0]);
-
-    var color = d3.scale.category10();
-
-    var xAxis = d3.svg.axis()
-        .scale(x)
-        .orient("bottom");
-
-    var yAxis = d3.svg.axis()
-        .scale(y)
-        .orient("left");
-
-    var svg = d3.select("#vis-canvas").append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-      .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-    d3.csv("data.csv", function(error, data) {
-      if (error) throw error;
-
-      data.forEach(function(d) {
-        d.MPG = +d.MPG;
-        d.PPG = +d.PPG;
-      });
-
-      x.domain(d3.extent(data, function(d) { return d.MPG; })).nice();
-      y.domain(d3.extent(data, function(d) { return d.PPG; })).nice();
-
-      svg.append("g")
-          .attr("class", "x axis")
-          .attr("transform", "translate(0," + height + ")")
-          .call(xAxis)
-        .append("text")
-          .attr("class", "label")
-          .attr("x", width)
-          .attr("y", -6)
-          .style("text-anchor", "end")
-          .text("MPG");
-
-      svg.append("g")
-          .attr("class", "y axis")
-          .call(yAxis)
-        .append("text")
-          .attr("class", "label")
-          .attr("transform", "rotate(-90)")
-          .attr("y", 6)
-          .attr("dy", ".71em")
-          .style("text-anchor", "end")
-          .text("PPG")
-
-      svg.selectAll(".dot")
-          .data(data)
-        .enter().append("circle")
-          .attr("class", "dot")
-          .attr("r", 3.5)
-          .attr("cx", function(d) { return x(d.MPG); })
-          .attr("cy", function(d) { return y(d.PPG); })
-          .style("fill", function(d) { return color(d.Name); });
-
-      var legend = svg.selectAll(".legend")
-          .data(color.domain())
-        .enter().append("g")
-          .attr("class", "legend")
-          .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
-
-      legend.append("rect")
-          .attr("x", width - 18)
-          .attr("width", 18)
-          .attr("height", 18)
-          .style("fill", color);
-
-      legend.append("text")
-          .attr("x", width - 24)
-          .attr("y", 9)
-          .attr("dy", ".35em")
-          .style("text-anchor", "end")
-          .text(function(d) { return d; });
-
-    });
