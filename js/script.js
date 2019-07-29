739395751af68128d72408d58263ccadb8a1d111
bpalakkal <47097307+bpalakkal@users.noreply.github.com>
2019-07-29 11:15:23 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -89,7 +89,6 @@ d3.json('data.json',function (data) {
         d.Team = d.Team;
 	d.Position = d.Position;
 	d.PPG = +d.PPG })
-}
   	ppg = tabulate(data,columns)
 	ppg.select("tbody").selectAll("tr")	
 	.sort(function(a,b){
