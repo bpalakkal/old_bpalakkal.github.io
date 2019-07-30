8d0b3d370a4c1e492483118f2e9ae160718aeb52
bpalakkal <47097307+bpalakkal@users.noreply.github.com>
2019-07-29 23:42:24 -0400

    Update script_ppg.js

--- js/script_ppg.js
+++ js/script_ppg.js
@@ -76,111 +76,104 @@ d3.json('data.json',function (data) {
 		return d3.descending(parseFloat(a.PPG), parseFloat(b.PPG))})
 });
 
-var margin = {top: 20, right: 20, bottom: 30, left: 400},
-    width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
-var svg = d3.select("body").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-// setup x 
-var xScale = d3.scale.linear().range([0, width]),
-    xAxis = d3.svg.axis().scale(xScale).orient("bottom");
-// setup y
-var yScale = d3.scale.linear().range([height, 0]),
-    yAxis = d3.svg.axis().scale(yScale).orient("left");
-// x-axis
-svg.append("g")
-    .attr("class", "x axis")
-    .attr("transform", "translate(0," + height + ")")
-    .call(xAxis)
-  .append("text")
-    .attr("class", "label")
-    .attr("x", width)
-    .attr("y", -6)
-    .style("text-anchor", "end")
-    .text("MPG");
-// y-axis
-svg.append("g")
-    .attr("class", "y axis")
-    .call(yAxis)
-  .append("text")
-    .attr("class", "label")
-    .attr("transform", "rotate(-90)")
-    .attr("y", 6)
-    .attr("dy", ".71em")
-    .style("text-anchor", "end")
-    .text("PPG");
-
-
-d3.csv("data.csv", function(d) {
-  return {
-    MPG : +d.MPG,
-    PPG : +d.PPG,
-    Name : d.Name    
-  };
-}, function(error, data) {
-   // update scales
-   xScale.domain([d3.min(data, xValue)-1, d3.max(data, xValue)+1]);
-   yScale.domain([d3.min(data, yValue)-1, d3.max(data, yValue)+1]);
-   // update x-axis
-   svg.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis)
-   .append("text")
-      .attr("class", "label")
-      .attr("x", width)
-      .attr("y", -6)
-      .style("text-anchor", "end")
-      .text("MPG");
-   // update y-axis
-   svg.append("g")
-      .attr("class", "y axis")
-      .call(yAxis)
-   .append("text")
-      .attr("class", "label")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 6)
-      .attr("dy", ".71em")
-      .style("text-anchor", "end")
-      .text("PPG");
-
-svg.selectAll(".dot")
-      .data(data)
-   .enter().append("circle")
-      .attr("class", "dot")
-      .attr("r", function(d) { return d["Name"] == 0 ? 0 : d["Name"]})
-      .attr("cx", xMap)
-      .attr("cy", yMap)
-      .style("fill", function(d) { return color(cValue(d));})
-      .on("mouseover", function(d) {
-         tooltip.transition()
-            .duration(200)
-            .style("opacity", .9);
-         tooltip.html("Message with " + d.MPG)
-            .style("left", (d3.event.pageX + 5) + "px")
-            .style("top", (d3.event.pageY - 28) + "px");
-      })
-      .on("mouseout", function(d) {
-         tooltip.transition()
-            .duration(500)
-            .style("opacity", 0);
-      });	
+var playerTeam = d3.json('data.json',function (data) {
+	data.forEach(function(d) {
+        d.Name = d.Name;
+        d.Team = d.Team;
+	d.Position = d.Position;
+	d.PPG = +d.PPG;
+	d.MPG = +d.MPG})
 });
-// setup x
-var xValue = function(d) { return d.MPG;},
-   xScale = d3.scale.linear().range([0, width]),
-   xMap = function(d) { return xScale(xValue(d));},
-   xAxis = d3.svg.axis().scale(xScale).orient("bottom");
-// setup y
-var yValue = function(d) { return d.PPG;},
-   yScale = d3.scale.linear().range([height, 0]),
-   yMap = function(d) { return yScale(yValue(d));},
-   yAxis = d3.svg.axis().scale(yScale).orient("left");
-var cValue = function(d) { return d["Name"];},
-   color = d3.scale.category20();
-var tooltip = d3.select("body").append("div")
-   .attr("class", "tooltip")
-   .style("opacity", 0);
+
+showScatterPlot(playerTeam)
+	
+function showScatterPlot(data) {
+    // just to have some space around items. 
+    var margins = {
+        "left": 40,
+            "right": 30,
+            "top": 30,
+            "bottom": 30
+    };
+    
+    var width = 500;
+    var height = 500;
+    
+    // this will be our colour scale. An Ordinal scale.
+    var colors = d3.scale.category10();
+
+    // we add the SVG component to the scatter-load div
+    var svg = d3.select("body").append("svg").attr("width", width).attr("height", height).append("g")
+        .attr("transform", "translate(" + margins.left + "," + margins.top + ")");
+
+    // this sets the scale that we're using for the X axis. 
+    // the domain define the min and max variables to show. In this case, it's the min and max prices of items.
+    // this is made a compact piece of code due to d3.extent which gives back the max and min of the price variable within the dataset
+    var x = d3.scale.linear()
+        .domain(d3.extent(data, function (d) {
+        return d.MPG;
+    }))
+    // the range maps the domain to values from 0 to the width minus the left and right margins (used to space out the visualization)
+        .range([0, width - margins.left - margins.right]);
+
+    // this does the same as for the y axis but maps from the rating variable to the height to 0. 
+    var y = d3.scale.linear()
+        .domain(d3.extent(data, function (d) {
+        return d.PPG;
+    }))
+    // Note that height goes first due to the weird SVG coordinate system
+    .range([height - margins.top - margins.bottom, 0]);
+
+    // we add the axes SVG component. At this point, this is just a placeholder. The actual axis will be added in a bit
+    svg.append("g").attr("class", "x axis").attr("transform", "translate(0," + y.range()[0] + ")");
+    svg.append("g").attr("class", "y axis");
+
+    // this is our X axis label. Nothing too special to see here.
+    svg.append("text")
+        .attr("fill", "#414241")
+        .attr("text-anchor", "end")
+        .attr("x", width / 2)
+        .attr("y", height - 35)
+        .text("Minutes Per Game");
+
+
+    // this is the actual definition of our x and y axes. The orientation refers to where the labels appear - for the x axis, below or above the line, and for the y axis, left or right of the line. Tick padding refers to how much space between the tick and the label. There are other parameters too - see https://github.com/mbostock/d3/wiki/SVG-Axes for more information
+    var xAxis = d3.svg.axis().scale(x).orient("bottom").tickPadding(2);
+    var yAxis = d3.svg.axis().scale(y).orient("left").tickPadding(2);
+
+    // this is where we select the axis we created a few lines earlier. See how we select the axis item. in our svg we appended a g element with a x/y and axis class. To pull that back up, we do this svg select, then 'call' the appropriate axis object for rendering.    
+    svg.selectAll("g.y.axis").call(yAxis);
+    svg.selectAll("g.x.axis").call(xAxis);
+
+    // now, we can get down to the data part, and drawing stuff. We are telling D3 that all nodes (g elements with class node) will have data attached to them. The 'key' we use (to let D3 know the uniqueness of items) will be the name. Not usually a great key, but fine for this example.
+    var player = svg.selectAll("g.node").data(data, function (d) {
+        return d.Name;
+    });
+
+    // we 'enter' the data, making the SVG group (to contain a circle and text) with a class node. This corresponds with what we told the data it should be above.
+    
+    var playergroup = player.enter().append("g").attr("class", "node")
+    // this is how we set the position of the items. Translate is an incredibly useful function for rotating and positioning items 
+    .attr('transform', function (d) {
+        return "translate(" + x(d.MPG) + "," + y(d.PPG) + ")";
+    });
+
+    // we add our first graphics element! A circle! 
+    chocolateGroup.append("circle")
+        .attr("r", 5)
+        .attr("class", "dot")
+        .style("fill", function (d) {
+            // remember the ordinal scales? We use the colors scale to get a colour for our manufacturer. Now each node will be coloured
+            // by who makes the chocolate. 
+            return colors(d.Name);
+    });
+
+    // now we add some text, so we can see what each item is.
+    chocolateGroup.append("text")
+        .style("text-anchor", "middle")
+        .attr("dy", -10)
+        .text(function (d) {
+            // this shouldn't be a surprising statement.
+            return d.Name;
+    });
+}
