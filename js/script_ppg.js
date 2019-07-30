77eded8f5ac97b025eeff4ff77158fd19d49b22a
bpalakkal <47097307+bpalakkal@users.noreply.github.com>
2019-07-30 00:00:04 -0400

    Update script_ppg.js

--- js/script_ppg.js
+++ js/script_ppg.js
@@ -76,7 +76,7 @@ d3.json('data.json',function (data) {
 		return d3.descending(parseFloat(a.PPG), parseFloat(b.PPG))})
 });
 
-var playerTeam = d3.json('data.json',function (data) {
+d3.json('data.json',function (data) {
 	data.forEach(function(d) {
         d.Name = d.Name;
         d.Team = d.Team;
@@ -85,95 +85,115 @@ var playerTeam = d3.json('data.json',function (data) {
 	d.MPG = +d.MPG})
 });
 
-showScatterPlot(playerTeam)
-	
-function showScatterPlot(data) {
-    // just to have some space around items. 
-    var margins = {
-        "left": 40,
-            "right": 30,
-            "top": 30,
-            "bottom": 30
-    };
-    
-    var width = 500;
-    var height = 500;
-    
-    // this will be our colour scale. An Ordinal scale.
-    var colors = d3.scale.category10();
-
-    // we add the SVG component to the scatter-load div
-    var svg = d3.select("body").append("svg").attr("width", width).attr("height", height).append("g")
-        .attr("transform", "translate(" + margins.left + "," + margins.top + ")");
-
-    // this sets the scale that we're using for the X axis. 
-    // the domain define the min and max variables to show. In this case, it's the min and max prices of items.
-    // this is made a compact piece of code due to d3.extent which gives back the max and min of the price variable within the dataset
-    var x = d3.scale.linear()
-        .domain(d3.extent(data, function (d) {
-        return d.MPG;
-    }))
-    // the range maps the domain to values from 0 to the width minus the left and right margins (used to space out the visualization)
-        .range([0, width - margins.left - margins.right]);
-
-    // this does the same as for the y axis but maps from the rating variable to the height to 0. 
-    var y = d3.scale.linear()
-        .domain(d3.extent(data, function (d) {
-        return d.PPG;
-    }))
-    // Note that height goes first due to the weird SVG coordinate system
-    .range([height - margins.top - margins.bottom, 0]);
-
-    // we add the axes SVG component. At this point, this is just a placeholder. The actual axis will be added in a bit
-    svg.append("g").attr("class", "x axis").attr("transform", "translate(0," + y.range()[0] + ")");
-    svg.append("g").attr("class", "y axis");
-
-    // this is our X axis label. Nothing too special to see here.
-    svg.append("text")
-        .attr("fill", "#414241")
-        .attr("text-anchor", "end")
-        .attr("x", width / 2)
-        .attr("y", height - 35)
-        .text("Minutes Per Game");
+var margin = {top: 20, right: 10, bottom: 20, left:10};
+var width = 960 - margin.left - margin.right;
+var height = 500 - margin.top - margin.bottom;
+var svg = d3.select('body')
+	.append('svg')
+	.attr('width', width + margin.left + margin.right)
+	.attr('height', height + margin.top + margin.bottom)
+	.append('g')
+	.attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
 
-    // this is the actual definition of our x and y axes. The orientation refers to where the labels appear - for the x axis, below or above the line, and for the y axis, left or right of the line. Tick padding refers to how much space between the tick and the label. There are other parameters too - see https://github.com/mbostock/d3/wiki/SVG-Axes for more information
-    var xAxis = d3.svg.axis().scale(x).orient("bottom").tickPadding(2);
-    var yAxis = d3.svg.axis().scale(y).orient("left").tickPadding(2);
+var xScale = d3.scaleLinear()
+		.range([0, width]);
 
-    // this is where we select the axis we created a few lines earlier. See how we select the axis item. in our svg we appended a g element with a x/y and axis class. To pull that back up, we do this svg select, then 'call' the appropriate axis object for rendering.    
-    svg.selectAll("g.y.axis").call(yAxis);
-    svg.selectAll("g.x.axis").call(xAxis);
+var yScale = d3.scaleLinear()
+		.range([height, 0]);
 
-    // now, we can get down to the data part, and drawing stuff. We are telling D3 that all nodes (g elements with class node) will have data attached to them. The 'key' we use (to let D3 know the uniqueness of items) will be the name. Not usually a great key, but fine for this example.
-    var player = svg.selectAll("g.node").data(data, function (d) {
-        return d.Name;
-    });
+var radius = d3.scaleSqrt()
+		.range([2,5]);
 
-    // we 'enter' the data, making the SVG group (to contain a circle and text) with a class node. This corresponds with what we told the data it should be above.
-    
-    var playergroup = player.enter().append("g").attr("class", "node")
-    // this is how we set the position of the items. Translate is an incredibly useful function for rotating and positioning items 
-    .attr('transform', function (d) {
-        return "translate(" + x(d.MPG) + "," + y(d.PPG) + ")";
-    });
+var xAxis = d3.axisBottom()
+		.scale(xScale);
 
-    // we add our first graphics element! A circle! 
-    chocolateGroup.append("circle")
-        .attr("r", 5)
-        .attr("class", "dot")
-        .style("fill", function (d) {
-            // remember the ordinal scales? We use the colors scale to get a colour for our manufacturer. Now each node will be coloured
-            // by who makes the chocolate. 
-            return colors(d.Name);
-    });
+var yAxis = d3.axisLeft()
+		.scale(yScale);
 
-    // now we add some text, so we can see what each item is.
-    chocolateGroup.append("text")
-        .style("text-anchor", "middle")
-        .attr("dy", -10)
-        .text(function (d) {
-            // this shouldn't be a surprising statement.
-            return d.Name;
-    });
-}
+var color = d3.scaleOrdinal(d3.schemeCategory20);
+
+xScale.domain(d3.extent(data, function(d){
+			return d.MPG;
+		})).nice();
+
+yScale.domain(d3.extent(data, function(d){
+			return d.PPG;
+		})).nice();
+
+radius.domain(d3.extent(data, function(d){
+			return d.PPG;
+		})).nice();
+
+svg.append('g')
+	.attr('transform', 'translate(0,' + height + ')')
+	.attr('class', 'x axis')
+	.call(xAxis);
+
+svg.append('g')
+	.attr('transform', 'translate(0,0)')
+	.attr('class', 'y axis')
+	.call(yAxis);
+
+var bubble = svg.selectAll('.bubble')
+	.data(data)
+	.enter().append('circle')
+	.attr('class', 'bubble')
+	.attr('cx', function(d){return xScale(d.MPG);})
+	.attr('cy', function(d){ return yScale(d.PPG); })
+	.attr('r', function(d){ return radius(d.PPG); })
+	.style('fill', function(d){ return color(d.Name); });
+
+	bubble.append('title')
+		.attr('x', function(d){ return radius(d.PetalLength); })
+		.text(function(d){
+		return d.Name;
+			});
+
+	svg.append('text')
+			.attr('x', 10)
+			.attr('y', 10)
+			.attr('class', 'label')
+			.text('Points Per Game');
+
+	var legend = svg.selectAll('legend')
+			.data(color.domain())
+			.enter().append('g')
+			.attr('class', 'legend')
+			.attr('transform', function(d,i){ return 'translate(0,' + i * 20 + ')'; });
+
+		// give x value equal to the legend elements. 
+		// no need to define a function for fill, this is automatically fill by color.
+		legend.append('rect')
+			.attr('x', width)
+			.attr('width', 18)
+			.attr('height', 18)
+			.style('fill', color);
+
+
+		svg.append('text')
+			.attr('x', width)
+			.attr('y', height - 10)
+			.attr('text-anchor', 'end')
+			.attr('class', 'label')
+			.text('Minutes Per Game');
+
+		
+legend.append('text')
+			.attr('x', width - 6)
+			.attr('y', 9)
+			.attr('dy', '.35em')
+			.style('text-anchor', 'end')
+			.text(function(d){ return d; });
+		
+legend.on('click', function(type){
+			d3.selectAll('.bubble')
+				.style('opacity', 0.15)
+				.filter(function(d){
+					return d.Name == type;
+				})
+				.style('opacity', 1);
+		})
+
+
+	})
