7af89b828449c0b3102724b1e6fa9253c46d2ef4
bpalakkal <47097307+bpalakkal@users.noreply.github.com>
2019-07-31 13:51:24 -0400

    Update barchart.js

--- js/barchart.js
+++ js/barchart.js
@@ -1,70 +1,72 @@
-Highcharts.chart('container', {
-    chart: {
-        type: 'bar',
-        marginLeft: 150
-    },
-    title: {
-        text: 'Most popular ideas by April 2016'
-    },
-    subtitle: {
-        text: 'Source: <a href="https://highcharts.uservoice.com/forums/55896-highcharts-javascript-api">UserVoice</a>'
-    },
-    xAxis: {
-        type: 'category',
-        title: {
-            text: null
-        },
-        min: 0,
-        max: 4,
-        scrollbar: {
-            enabled: true
-        },
-        tickLength: 0
-    },
-    yAxis: {
-        min: 0,
-        max: 1200,
-        title: {
-            text: 'Votes',
-            align: 'high'
-        }
-    },
-    plotOptions: {
-        bar: {
-            dataLabels: {
-                enabled: true
-            }
-        }
-    },
-    legend: {
-        enabled: false
-    },
-    credits: {
-        enabled: false
-    },
-    series: [{
-        name: 'Votes',
-        data: [
-            ["Gantt chart", 1000],
-            ["Autocalculation and plotting of trend lines", 575],
-            ["Allow navigator to have multiple data series", 523],
-            ["Implement dynamic font size", 427],
-            ["Multiple axis alignment control", 399],
-            ["Stacked area (spline etc) in irregular datetime series", 309],
-            ["Adapt chart height to legend height", 278],
-            ["Export charts in excel sheet", 239],
-            ["Toggle legend box", 235],
-            ["Venn Diagram", 203],
-            ["Add ability to change Rangeselector position", 182],
-            ["Draggable legend box", 157],
-            ["Sankey Diagram", 149],
-            ["Add Navigation bar for Y-Axis in Highstock", 144],
-            ["Grouped x-axis", 143],
-            ["ReactJS plugin", 137],
-            ["3D surface charts", 134],
-            ["Draw lines over a stock chart, for analysis purpose", 118],
-            ["Data module for database tables", 118],
-            ["Draggable points", 117]
-        ]
-    }]
-});
+      const xValue = d => d.PPG;
+      const xLabel = 'Points Per Game';
+      const yValue = d => d.Names;
+      const yLabel = 'Player Names';
+      const margin = { left: 150, right: 30, top: 5, bottom: 75 };
+
+      const svg = d3.select('body);
+      const width = svg.attr('width');
+      const height = svg.attr('height');
+      const innerWidth = width - margin.left - margin.right;
+      const innerHeight = height - margin.top - margin.bottom;
+
+      const g = svg.append('g')
+          .attr('transform', `translate(${margin.left},${margin.top})`);
+      const xAxisG = g.append('g')
+          .attr('transform', `translate(0, ${innerHeight})`);
+      const yAxisG = g.append('g');
+
+      xAxisG.append('text')
+          .attr('class', 'axis-label')
+          .attr('x', innerWidth / 2)
+          .attr('y', 55)
+          .text(xLabel);
+
+      const xScale = d3.scaleLinear();
+      const yScale = d3.scaleBand()
+        .paddingInner(0.3)
+        .paddingOuter(0);
+
+      const xTicks = 10;
+      const xAxis = d3.axisBottom()
+        .scale(xScale)
+        .ticks(xTicks)
+        .tickPadding(5)        
+        .tickSize(-innerHeight);
+
+      const yAxis = d3.axisLeft()
+        .scale(yScale)
+        .tickPadding(5)
+        .tickSize(-innerWidth);
+
+      const row = d => {
+        const PPG = +d.PPG;
+        return {
+          Player: d.Name,
+          PPG: +d.PPG
+        };
+      };
+
+      d3.csv('data.csv', row, data => {
+        yScale
+          .domain(data.map(yValue).reverse())
+          .range([innerHeight, 0]);
+
+        xScale
+          .domain([0, d3.max(data, xValue)])
+          .range([0, innerWidth])
+          .nice(xTicks);
+
+        g.selectAll('rect').data(data)
+          .enter().append('rect')
+            .attr('x', 0)
+            .attr('y', d => yScale(yValue(d)))
+            .attr('width', d => xScale(xValue(d)))
+            .attr('height', d => yScale.bandwidth())
+            .attr('fill', 'steelblue');
+
+        xAxisG.call(xAxis);
+
+        yAxisG.call(yAxis);
+        yAxisG.selectAll('.tick line').remove();
+      });
