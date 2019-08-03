5b67bff0d3572b92930543b0655b4448fcd88aec
bpalakkal <47097307+bpalakkal@users.noreply.github.com>
2019-08-03 16:03:45 -0400

    Update ppg.js

--- js/ppg.js
+++ js/ppg.js
@@ -4,7 +4,7 @@ var clicks = {name: 0, team: 0, position: 0,games_played: 0,ppg:0};
 
 // draw the table
 var margin = {top: 40, right: 20, bottom: 10, left: 10}
-var width = 800 - margin.left - margin.right, height = 400 - margin.top - margin.bottom	
+var width = 1280 - margin.left - margin.right, height = 400 - margin.top - margin.bottom	
 var svg = d3.select("body").select(".chartArea").append("svg")
       .attr("width", width + margin.left + margin.right)
       .attr("height", height + margin.top + margin.bottom)      
@@ -13,8 +13,8 @@ var svg = d3.select("body").select(".chartArea").append("svg")
      
 
 var forobj = svg.append("foreignObject")
-      .attr("width", 430)
-      .attr("height", 740)
+      .attr("width", 1250)
+      .attr("height", 350)
       .append("xhtml:body")
 
 forobj.append("div")
