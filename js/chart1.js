8e0a405bcec0c44764fd1b0ac5a2a1b1eaeee380
bpalakkal <47097307+bpalakkal@users.noreply.github.com>
2019-08-04 16:31:17 -0400

    Update chart1.js

--- js/chart1.js
+++ js/chart1.js
@@ -25,7 +25,7 @@ var html = '<div class="row" style="margin-right: 15px;margin-left: 15px;margin-
             '<div class="col" style="max-width: 30%;"> <img style="height:200px" src = "' + filteredData[0]["imgURL"] +'"></img> </div>'+
             '<div class="col" ><text font-size:16px> Shot Attempts </text> <div id="chart"> </div> </div> ' +
       '<div class="col" > <text font-size:16px> Shots Made </text> <div id="chart2"> </div> </div> ' +
-      '<div class="col" > <div id="chart3"> </div> </div> ' +
+      '<div class="col" > <text font-size: 16px>Shot Range<div id="chart3"> </div> </div> ' +
             '<div class="col" > <text font-size:16px> Player Stats </text> <div id="linechart"> </div> </div> ' 
       '</div>';
 
@@ -33,7 +33,7 @@ var html = '<div class="row" style="margin-right: 15px;margin-left: 15px;margin-
 //var svgid = document.getElementById("svg2")
 //svgid.insertAdjacentHTML('afterbegin',html);
 	document.getElementById("playerInfo").insertAdjacentHTML('afterbegin',html);
-displayLineChart (player);
+	displayLineChart (player);
 	 var newData ={};
 	newData.FTA =  parseInt(player["FTA"]);
 	newData["2PA"] = parseInt(player["2PA"]);
@@ -48,16 +48,32 @@ displayLineChart (player);
 	
 	 donut(newData_2, "#chart2", ["FT Made","2 Pt Made", "3 Pt Made"]);
 	
-	var newData_3 ={};
-	newData_3["FGPt"] =  parseInt(player["FGPt"]);
-	newData_3["5TO9FGPt"] =  parseInt(player["5TO9FGPt"]);
-	newData_3["10TO14FGPt"] = parseInt(player["10TO14FGPt"]);
-	newData_3["15TO19FGPt"] = parseInt(player["15TO19FGPt"]);
-	newData_3["20TO24FGPt"] = parseInt(player["20TO24FGPt"]);
-	newData_3["25TO29FGPt"] = parseInt(player["25TO29FGPt"]);
-	//barchart(newData_3, "#chart3", ["FT Made","2 Pt Made", "3 Pt Made"]);
-	
-	
+	var newData_arr =[];
+	 var newData_3 ={};
+	newData_3.key = "FGPt";
+	newData_3.value = parseInt(player["FGPt"]);
+	newData_arr.push(newData_3);
+	 var newData_3 ={};
+	newData_3.key = "5TO9FGPt";
+	newData_3.value =parseInt(player["5TO9FGPt"]);
+	newData_arr.push(newData_3);
+	 var newData_3 ={};
+	newData_3.key = "10TO14FGPt";
+	newData_3.value =parseInt(player["10TO14FGPt"]);
+	newData_arr.push(newData_3);
+	 var newData_3 ={};
+	newData_3.key = "15TO19FGPt";
+	newData_3.value =parseInt(player["15TO19FGPt"]);
+	newData_arr.push(newData_3);
+	 var newData_3 ={};
+	newData_3.key = "20TO24FGPt";
+	newData_3.value =parseInt(player["20TO24FGPt"]);
+	newData_arr.push(newData_3);
+	 var newData_3 ={};
+	newData_3.key = "25TO29FGPt";
+	newData_3.value =parseInt(player["25TO29FGPt"]);
+	newData_arr.push(newData_3);
+	barchart(newData_arr, "#chart3", ["FT Made","2 Pt Made", "3 Pt Made"]);
 	
    //document.write(html);
 })
