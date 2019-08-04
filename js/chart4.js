c96b9f3a31b9e8461f5338f3f694773578a36deb
bpalakkal <47097307+bpalakkal@users.noreply.github.com>
2019-08-04 16:41:13 -0400

    Update chart4.js

--- js/chart4.js
+++ js/chart4.js
@@ -7,24 +7,23 @@ d3.json("Roster.json", function(error, data) {
 	var newData = []; 
 	for (var key in filteredData[0]["ratings"][0]) {
 		var newObj ={};
-  			 switch (key) {
-     			case "spd":  newObj.Rating = "Speed"; 
-			case "pss": newObj.Rating = "Pass";
-      			case "dnk": newObj.Rating = "Dunk";
-      			case "ft": newObj.Rating = "Freet Throw";
-			case "fg": newObj.Rating = "Field Goals";
-			case "jmp": newObj.Rating = "Jump";
-      			case "reb": newObj.Rating = "Rebounding";
-      			case "hgt": newObj.Rating = "Height";
-			case "endu": newObj.Rating = "Endurance";
-			case "drb": newObj.Rating = "Def Rebounding";
-			case "oiq": newObj.Rating = "Off IQ";
-			case "tp": newObj.Rating = "Turnover %";
-			case "diq": newObj.Rating = "Def IQ";
-			case "stre": newObj.Rating = "Stretch";
-			case "ins": newObj.Rating = "Ins";
-			 }
-			
+			switch (key) {
+     			case "spd":  newObj.Rating = "Speed"; break;
+			case "pss": newObj.Rating = "Pass"; break;
+      			case "dnk": newObj.Rating = "Dunk"; break;
+      			case "ft": newObj.Rating = "Freet Throw";break;
+			case "fg": newObj.Rating = "Field Goals"; break;
+			case "jmp": newObj.Rating = "Jump"; break;
+      			case "reb": newObj.Rating = "Rebounding"; break;
+      			case "hgt": newObj.Rating = "Height"; break;
+			case "endu": newObj.Rating = "Endurance"; break;
+			case "drb": newObj.Rating = "Def Rebounding"; break;
+			case "oiq": newObj.Rating = "Off IQ"; break;
+			case "tp": newObj.Rating = "Turnover %"; break;
+			case "diq": newObj.Rating = "Def IQ"; break;
+			case "stre": newObj.Rating = "Stretch"; break;
+			case "ins": newObj.Rating = "Ins"; break;
+			 }		
     
 		newObj.RatingValue =parseInt( filteredData[0]["ratings"][0][key]); 
 		newData.push(newObj);
